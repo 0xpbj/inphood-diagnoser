ffb9293414db929df5437ca0772a152a3ff65c45
Alex Carreira <aycarrei@gmail.com>
2017-09-26 19:37:43 -0700

    Integrate and test dotenv.

--- bot.js
+++ bot.js
@@ -1,8 +1,22 @@
+let dotEnv = require('dotenv').config({path: './.env-production'})
+
 var botBuilder = require('claudia-bot-builder'),
     excuse = require('huh');
 
+const firebase = require('firebase')
+if (firebase.apps.length === 0) {
+  firebase.initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+  })
+}
+
 module.exports = botBuilder(function (request) {
-  return 'Thanks for sending ' + request.text  + 
-      '. Your message is very important to us, but ' + 
+
+  return 'Thanks for sending ' + request.text  +
+      '. Your message is very important to us, but ' +
       excuse.get();
 }, { platforms: ['facebook', 'twilio', 'viber', 'telegram', 'kik', 'groupme'] })
